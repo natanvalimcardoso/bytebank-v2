ac519258f5073b9e7c07fdcd348b6867336295c3
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-02-15 15:44:51 -0300

    dia 1

---
+++ lib/myapp.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'screens/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
